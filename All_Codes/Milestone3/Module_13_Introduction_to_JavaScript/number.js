951aeea732a8bb43c9d6c3656d6020682473139b
injamul007 <injamul007hoque@gmail.com>
2025-08-08 15:59:42 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/number.js
@@ -0,0 +1,42 @@
+//? Number in JS
+
+var orangePrice = 20;
+// console.log(typeof orangePrice);
+
+// var applePrice = "30.4";
+// var applePrice = "HelloHello23";
+// var applePrice = "23";
+// var applePrice = 23;
+// var applePrice = parseInt("Twenty");
+var applePrice1 = Number("20.3");
+var applePrice2 = parseInt("20.3");
+// var applePrice3 = parseFloat("20.3");
+var applePrice3 = Number("20.3");
+var applePrice4 = 20.3;
+var applePrice5 = Number("Hello");
+// console.log(typeof applePrice);
+// console.log(applePrice1);
+// console.log(applePrice2);
+console.log(isNaN(applePrice3));
+console.log(isNaN(applePrice4));
+console.log(isNaN(applePrice5));
+// console.log(orangePrice + applePrice);
+// console.log(isNaN(applePrice));
+// console.log(orangePrice + isNaN(applePrice));
+// console.log(orangePrice + parseFloat(applePrice));
+// console.log(orangePrice + parseInt(applePrice));
+// console.log(orangePrice + Number(applePrice));
+
+// var convertStringToNumber = Number(applePrice);
+// console.log(typeof convertStringToNumber);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+// console.log(Number(total.toFixed(1)));
+
+const num = 50 / 0;
+
+// console.log(num);
+
