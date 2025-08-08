856c0f614a6459c4798f53f3f51ffaff61e264a6
injamul007 <injamul007hoque@gmail.com>
2025-08-08 16:00:31 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/types.js
@@ -0,0 +1,29 @@
+//! Types of variables
+
+//! string type of variable
+
+var name = "Boss Khan";
+
+// console.log(name);
+
+var address = "Andar killa bandar ban with some katabon";
+
+// console.log(address);
+
+var country = "Bangladesh";
+
+// console.log(country);
+
+
+//! Boolean types of variable
+
+var isRich = true;
+// console.log(isRich)
+
+var isPassed = true;
+// console.log(isPassed)
+// console.log(typeof isPassed)
+
+//! Number types of variable
+var money = 500;
+console.log(typeof money);
