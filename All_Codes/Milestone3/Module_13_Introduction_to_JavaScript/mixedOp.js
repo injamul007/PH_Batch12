713ab1e5f165a49febf2a50d5f6050ad73ed984e
injamul007 <injamul007hoque@gmail.com>
2025-08-08 15:59:05 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/mixedOp.js
@@ -0,0 +1,20 @@
+//? Mixed operation in js
+
+// console.log(20 + "10"); //? this will not count as calculation bcz right side value is string rather than this will concat it like strings.
+// console.log("10" + 20); //? this will not count as calculation bcz left side value is string rather than this will concat it like strings.
+
+
+var firstName = "Boss";
+var lastName = " BD";
+var fullName = firstName - lastName;
+
+// console.log(fullName); 
+
+// console.log(40 + '2');
+console.log(40 - '2');
+
+
+//? Task 1
+// what is null ?? example please !
+//? Task 2
+// What is isNaN ?? example Please !
