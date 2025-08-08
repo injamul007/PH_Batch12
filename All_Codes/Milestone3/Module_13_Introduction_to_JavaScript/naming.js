cc02973282a7277f46fd0a5390226b3877945c80
injamul007 <injamul007hoque@gmail.com>
2025-08-08 15:59:22 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/naming.js
@@ -0,0 +1,30 @@
+//! Naming in JS
+
+//? 1. you cannot use keyword in variable name
+// var false = 45; 
+
+//? 2. you cannot use space or gap in variable name
+// var is happy = false;
+
+//? 3. you cannot use quote in variable name
+// var "address" = "kochu khet"
+
+//? 4. you cannot start with a number. but number other than the first letter is allowed.
+// var 3money = 55; 
+// var money3 = 55; //? this one is allowed
+
+//? JS (name) is case sensitive 
+// address
+// Address 
+// ADDRESS
+// addRess 
+// ADDress 
+
+
+//? How to write a long variable name
+
+// var my current home address = "Bangladesh";
+// var my_current_home_address = "Bangladesh"; //?snake case
+// var myCurrentHomeAddress = "Bangladesh"; //?camel case
+// var MyCurrentHomeAddress = "Bangladesh"; //? pascal case
+
