361ef13a19356d84c70f90674e25eae9d0cb4521
injamul007 <injamul007hoque@gmail.com>
2025-08-08 16:00:15 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/shorthand.js
@@ -0,0 +1,15 @@
+//? Shorthand in JS
+
+var price = 35;
+// price = price + 10;
+price += 10; //? price = price + 10
+
+// console.log(price)
+
+var fridge = 40000;
+fridge -= 2000;
+
+// console.log(fridge);
+
+
+
