a53533c28d865a5aaa7ae777dac433361c9170c9
injamul007 <injamul007hoque@gmail.com>
2025-08-08 15:58:49 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/math.js
@@ -0,0 +1,17 @@
+/**
+****************************************************
+*               MATH NEEDED FOR JAVASCRIPT
+****************************************************
+* 1. Number: +ve, -ve, 0 [50, -9, 0]
+* 2. Integer: 2, 88, [odd, even]: 2, 4, 897
+* 3. Float: 5.6, 2.2
+* 4. Operation: +, -, *, /
+* 5. More Operation: %, []
+* 6. Percentage: 5%, 50 taka er 10% koto?
+* 7. Sum: 12,14,16
+* 8. Average: 12,14,16
+* 9. Max/Min: 12,14,16
+* 10. Series:
+* 11. Area: Triangle
+* 12. Prime number
+*/
