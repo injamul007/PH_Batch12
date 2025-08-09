f41f7b19a353cff8ec3e3e4034240070d5f1f36f
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:53:30 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/variable.js
@@ -0,0 +1,34 @@
+//? Recap of Variables
+
+//? number
+
+var sunglassPrice = 80;
+
+//? string
+
+var sub = "EEE";
+
+//? Boolean
+
+var isPassed = true;
+
+/**  Naming Convention  
+ *  1. single word
+ *  2. no quote
+ *  3. no Gap or dash
+ *  4. cannot start with number
+ *  5. cannot be a keyword
+ *  6. prefer camelCase
+ * **/
+
+
+var lastExamResult = 89;
+
+/** 
+ *  Operation
+ * 1. +, -, *, /, %
+ * 2. isNaN?
+ * 3. null
+ * 4. Number
+ * 5. +=, -=, *=, /=
+ */
