9f764a134f3683a085d1e3a883de4ff01fb637d5
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:52:42 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/shortcut.js
@@ -0,0 +1,49 @@
+//? Logical not operator
+
+const isPassed = false;
+
+
+//? if (isPassed === false) //? same as (!isPassed)
+if (!isPassed) {
+  // console.log("Tore biya diya dimu")
+} else {
+  // console.log("tui amar noyoner moni")
+}
+
+
+// if (isLeader) {
+//   console.log("leader ashche ! chair charo")
+// } else {
+//   console.log("vitore dhukte dibi na")
+// }
+
+//? let isLogged = false;
+
+// if (!isLogged) {
+//   console.log("user is not logged in")
+// } else {
+//   console.log("user is logged in")
+// }
+
+//? let isEnable = true;
+
+// isEnable = !isEnable;
+
+// console.log(isEnable)
+
+//? let inputValue = null;
+
+// if (!inputValue) {
+//   console.log("input value is falsy");
+// } else {
+//   console.log("input value is truthy");
+// }
+
+
+//? let userInput = "someValue";
+
+// if (!userInput) {
+//   console.log("User input is empty");
+// } else {
+//   console.log("User input is:", userInput);
+// }
