06cb4045a5b30362395876ce18c0fab91c0b90e8
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:51:24 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/if.js
@@ -0,0 +1,44 @@
+/**
+ * tui jodi amake kahwas taile ami ashbo
+ * 
+ * --------------------
+ * 
+ * if you buy food for me then i will come
+ * ----------------------
+ * 
+ * if biriyani price is less than 300 then i will buy
+ * --------------------------
+ * 
+ * -----------STRUCTURE-----------------
+ * if (condition) {
+ *    ? this will execute if the above condition is true
+ * } else {
+ *   ? this will execute if the above condition is not true
+ * }
+ */
+
+
+
+if (13 < 10) {
+  // console.log("i have a smaller value")
+} else {
+  // console.log("i have not a smaller value")
+}
+
+var kacchi = 600;
+
+if (kacchi < 500) {
+  // console.log("mama give me some kacchi")
+} else {
+  // console.log("mam amar kase tk kom ase")
+}
+
+
+var weight = 20;
+
+if (weight <= 20) {
+  console.log("i will carry it by myself")
+} else {
+  console.log("i will rent a rickshaw");
+}
+
