02f2c0a67251730e29d7e05c83de188b781af291
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:51:42 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/multiple.js
@@ -0,0 +1,50 @@
+//?
+
+const salary = 25000;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+
+
+// if (salary > 20000 && height > 66) {
+//   console.log("Su ------- Patro");
+// } else {
+//   console.log("Onno patro khuji");
+// }
+
+
+// if (salary > 25000 || height > 72) {
+//   console.log("Eso baba Kobul")
+// } else {
+//   console.log("Bhag tui mokbul")
+// }
+
+
+//? more and more condition
+
+// if (salary > 25000 || height > 72 || isBCS == true) {
+//   console.log("Eso baba Kobul")
+// } else {
+//   console.log("Bhag tui mokbul")
+// }
+
+// if (salary > 24000 && height > 62 && isBCS == true) {
+//   console.log("Eso baba Kobul")
+// } else {
+//   console.log("Bhag tui mokbul")
+// }
+
+//? -------Complex Condition------------
+
+// if ((salary > 24000 || hasCar == true) && isBCS == true) {
+//   console.log("tomar 14 gostio raji")
+// } else {
+//   console.log("vag muri khaa")
+// }
+
+
+if ((salary > 24000 && hasCar == true) || isBCS == true) {
+  console.log("tomar 14 gostio raji")
+} else {
+  console.log("vag muri khaa")
+}
