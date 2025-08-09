f641d6e414c25699b8c99b7e5e07b445a3da17e4
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:51:09 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/comparison.js
@@ -0,0 +1,38 @@
+
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(10>=10);
+// console.log(10<=10);
+// console.log(15>=10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+
+// console.log(10 == "10"); //? == means Loose equal
+// console.log(10 === "10"); //? === means Strict equal
+// console.log(10 == 10.1); 
+
+
+var guavaPrice = 40;
+var applePrice = 200;
+
+// console.log(guavaPrice < applePrice);
+// console.log(guavaPrice >= applePrice);
+// console.log(guavaPrice != applePrice);
+
+
+/**
+ * In JS there are two types of equals like ----
+ * 1. == (double equal)  //? this also can be call loose equal
+ * 1. === (triple equal) //? this also can be call strict equal
+*/
+
+
+// console.log(10 == "10"); //? ==  Loose equal/value compare
+
+// console.log(10 === "10"); //? === Strict equal/data type compare
+
+// console.log(10 != "10"); //? !=   loose inEqual
+console.log(10 !== "10"); //? !==  Strict inEqual
+console.log(10 === "10"); //? ===  Strict Equal
