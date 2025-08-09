fbfeb2c1282d6a462a5fdb192ad4bcfdc6682c62
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:50:49 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/aunty.js
@@ -0,0 +1,14 @@
+/**
+ * COMPARISON
+ * 1. bigger: >
+ * 2. less: <
+ * 3. Equal: ==
+ * 4. greater than or equal: >=
+ * 5. less than or equal: <=
+ * 6. not equal: !=
+ * 
+ * ---------------
+ * 7.and: &&
+ * 8. or: ||
+ * 9. not: !
+ */
