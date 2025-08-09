daaaaa6bc8e62389cc668c3d4cba9356117d3219
injamul007 <injamul007hoque@gmail.com>
2025-08-09 14:51:54 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_14_JS_conditionals/nested.js
@@ -0,0 +1,17 @@
+//?
+
+const money = 0;
+
+if (money > 300) {
+  console.log("Bro you are rich !!!")
+} else {
+  if (money > 100) {
+    console.log("tui shala gorib o na abar fokir o na !")
+  } else {
+    if (money > 0) {
+      console.log("dosto kola kha r calcium bara")
+    } else {
+      console.log("amar kase tk dhar chaibi na !!")
+    }
+  }
+}
