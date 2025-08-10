11951ad9f2817574767594c04f2f72ab3b73717a
injamul007 <injamul007hoque@gmail.com>
2025-08-10 14:50:32 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_5_array/includes.js
@@ -0,0 +1,12 @@
+//?
+
+const friends = ["balam", "kalam", "salam", "gelam", "pailam"];
+
+// console.log(friends.includes("pailam"));
+
+if (friends.includes("kalam")) {
+  console.log("party");
+} else {
+  console.log("not party");
+}
+
