c2e12422b35af34ad04b2e2c50f36d50bb0606e5
injamul007 <injamul007hoque@gmail.com>
2025-08-10 14:52:20 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_5_array/practice_problem.js
@@ -0,0 +1,49 @@
+//?
+//? Problem 1
+
+const fruits = ["apple", "mango", "orange", "banana", "lichi"];
+
+// console.log(fruits[3]);
+
+// fruits[2] = "jambura";
+// console.log(fruits);
+
+//? Problem 2
+
+const touristDestinations = ["cox's bazar", "bandarban", "shajek"];
+
+touristDestinations.push('Nilshagor');
+touristDestinations.push('Nilgiri', 'rangamati');
+touristDestinations.pop();
+// console.log(touristDestinations)
+
+//? Problem 3
+
+const differentBook = ["C++", "Java", "Javascript", "python"];
+
+// console.log(differentBook.includes('Javascript'));
+// console.log("Yes this element is present in this array")
+
+
+//? Problem 4
+
+const nums = [];
+
+const age = 30;
+
+const studentRoll = [1,2,3,4,5]
+
+// console.log(Array.isArray(nums), "this is an array");
+// console.log(Array.isArray(age), "this is not an array");
+// console.log(Array.isArray(studentRoll), "this is an array");
+
+//? Problem 5
+
+const arr1 = [1,2,3,4];
+const arr2 = [5,6,7,8];
+
+const concatArr = arr1.concat(arr2);
+
+console.log(arr1)
+console.log(arr2)
+console.log(concatArr)
