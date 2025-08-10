edd78f38ef0ea3d1baa3b04eb727a42324a989d8
injamul007 <injamul007hoque@gmail.com>
2025-08-10 14:51:29 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_5_array/isArray.js
@@ -0,0 +1,15 @@
+//?
+
+const friends = ["balam", "kalam", "salam", "gelam", "pailam"];
+
+const nums = [];
+
+const food = "ros o gol l a";
+
+const age = 30;
+
+console.log(Array.isArray(friends)); //? this is how you check is it a array or not !!
+console.log(Array.isArray(nums)); //? this is how you check is it a array or not !!
+console.log(Array.isArray(food));
+console.log(Array.isArray(age))
+
