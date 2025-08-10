cd3924dc8e648ad61f6ced2499754b5196ebd82d
injamul007 <injamul007hoque@gmail.com>
2025-08-10 14:50:57 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_5_array/index.js
@@ -0,0 +1,14 @@
+//?
+
+const numbers = [88, 77, 99, 421, 54, 63];
+
+// console.log(numbers[4]);
+//? get element value by index
+
+console.log(numbers);
+const fourth = numbers[4]
+console.log(fourth);
+
+//? set or update element value by index
+numbers[4] = 64;
+console.log(numbers[4]);
