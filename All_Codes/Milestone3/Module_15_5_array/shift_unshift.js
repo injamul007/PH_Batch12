d54459431a0a07d79285b3fb62b1957236592338
injamul007 <injamul007hoque@gmail.com>
2025-08-10 14:52:52 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_5_array/shift_unshift.js
@@ -0,0 +1,13 @@
+//?
+
+const friends = ["balam", "kalam", "salam", "gelam", "pailam"];
+
+
+console.log(friends);
+
+friends.shift();
+console.log(friends)
+
+friends.unshift("tulu","kulu");
+console.log(friends);
+
