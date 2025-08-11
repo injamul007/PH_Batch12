330755f4e3cd41e8afc526164ae75e6a6d7f177b
injamul007 <injamul007hoque@gmail.com>
2025-08-11 15:52:56 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/break.js
@@ -0,0 +1,17 @@
+//?
+
+// for (let i = 0; i< 15; i++) {
+//   console.log(i)
+//   if (i >= 10) {
+//     break;
+//   }
+// }
+
+let n = 54;
+while (n >= 24) {
+  console.log(n)
+  if (n <= 50) {
+    break;
+  }
+  n--;
+}
