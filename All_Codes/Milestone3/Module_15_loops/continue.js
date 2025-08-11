ee5fe4eeeb5a7e8671e49adf74f1bd950458b4aa
injamul007 <injamul007hoque@gmail.com>
2025-08-11 15:53:11 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/continue.js
@@ -0,0 +1,21 @@
+//?
+
+//? Continue --> Skip rest of the code for this iteration;
+//? Break --> I am done with this loop, loop end;
+
+// for (let i = 1; i<= 20; i++) {
+//   if (i % 2 === 1) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+let n = 0;
+
+while (n < 50) {
+  n++;
+  if (n % 5 !== 0) {
+    continue;
+  }
+  console.log(n)
+}
