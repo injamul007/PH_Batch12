f97f3a26025a4d8e9493bd284735f9b55bcb0f73
injamul007 <injamul007hoque@gmail.com>
2025-08-11 15:55:02 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/for_of.js
@@ -0,0 +1,23 @@
+//? basic array for stating loop concepts
+
+const numbers = [12, 98, 45, 63, 21, 72]
+
+// console.log(numbers)
+
+//? for of loop
+// for(const num of numbers) {
+//   console.log(num);
+// }
+
+//? for loop
+// for (let i = 0; i<numbers.length; i++) {
+  // console.log(i, numbers[i]);
+// }
+
+
+const fruits = ["orange", "apple", "banana", "jackfruit", "watermelon"]
+
+for (const fruit of fruits) {
+  console.log(fruit);
+  console.log("I want to eat")
+}
