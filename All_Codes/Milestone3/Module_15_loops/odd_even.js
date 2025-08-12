b36cb84a391d8f6bbdb1422082b59b744a1fd4bc
injamul007 <injamul007hoque@gmail.com>
2025-08-12 18:10:19 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/odd_even.js
@@ -0,0 +1,19 @@
+//?
+
+// for (let num = 0; num<=10; num++) {
+//   if (num % 2 === 0) {
+//     console.log("Even Num",num)
+//   }
+// }
+
+
+// for (let num = 1; num<=10; num++) {
+//   if (num % 2 === 1) {
+//     console.log("Odd Num",num)
+//   }
+// }
+
+
+for (let i = 1; i<=20; i+=2) {
+  console.log(i)
+}
