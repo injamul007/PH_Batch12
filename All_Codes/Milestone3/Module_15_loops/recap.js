ad6fafd62a61a35afe5e610d481f2b2a00f133a9
injamul007 <injamul007hoque@gmail.com>
2025-08-12 18:11:05 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/recap.js
@@ -0,0 +1,17 @@
+//? const : Value will not change
+//? let: value can change
+//? var: we should not use it
+
+//? Variable
+const amount = 200;
+
+//? Condition
+if(amount > 100) {
+
+} else {
+
+}
+
+//? array
+const numbers = [12, 32, 54, 66];
+
