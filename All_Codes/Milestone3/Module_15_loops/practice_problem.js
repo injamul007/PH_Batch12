81587ccf5e57a029c9b2ec5e72ee9c226dd75ea3
injamul007 <injamul007hoque@gmail.com>
2025-08-12 18:10:37 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/practice_problem.js
@@ -0,0 +1,94 @@
+//? loop practice problem
+//? break;
+//? 1. Century 
+
+// for (let i = 1; i<=200; i++) {
+//   if (i === 100) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+
+//? 2. sum upto 100
+
+// var num = 1;
+// var sum = 0;
+
+// while (num < 50) {
+//   sum += num;
+//   num++
+//   if (sum > 100) {
+//     break;
+//   }
+//   console.log(sum);
+// }
+
+
+//? square number break
+
+// for (var i = 1; i <= 50; i++) {
+//   if (i*i === 16) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+
+//? continue
+
+// for(let i = 1; i <= 40; i++) {
+//   if(i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+//? Display odd numbers
+
+// for (let i = 55; i <= 85; i+=2 ) {
+//   if(i % 5 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+//? Find all the odd number between 61 to 100;
+
+// for (let i = 61; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+
+//? Find all the odd number between 78 to 100;
+
+// for (let i = 78; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+
+//? display sum of all the odd numbers from 91 to 129;
+
+// let sum = 0;
+
+// for (let i = 91; i <= 129; i++) {
+//   if (i % 2 !== 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+//? Generate a multiplication table for number 9
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`9 x ${i} = ${9 * i}`);
+// }
+
+//? Implement a countdown timer that counts down from 81 to 65.
+
