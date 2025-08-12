0f7c1831ce8309f6e50b556738e0674680199d65
injamul007 <injamul007hoque@gmail.com>
2025-08-12 18:11:35 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/while.js
@@ -0,0 +1,16 @@
+//?
+
+const number = 7
+if (number < 10) {
+  console.log("Choto ekta number paisi")
+}
+
+console.log("Something new is cooking")
+
+
+//? -----------While loop-------------
+const num = 0; //? Loop variable
+while (num < 5) {
+  console.log("looping", num);
+  num++;
+}
