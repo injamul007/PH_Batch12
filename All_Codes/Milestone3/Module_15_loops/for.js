d1b4d465f452013753c4cd937583d9210bbb89e3
injamul007 <injamul007hoque@gmail.com>
2025-08-12 18:10:05 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_15_loops/for.js
@@ -0,0 +1,17 @@
+//?
+
+// for (let i = 1; i<= 10; i++) {
+//   // console.log(i);
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+
+// let sum = 0;
+// for(let i = 1; i<=10; i++) {
+//   sum += i;
+//   console.log(sum)
+// }
+
+
