84b3b9a4911a8444f6c08fa9f123dafcb56c1b8a
injamul007 <injamul007hoque@gmail.com>
2025-08-13 13:47:30 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/case.js
@@ -0,0 +1,31 @@
+//? Case sensitive In String in JS
+
+const school = "RAJ UK Uttara Model School";
+
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+//? Uppercase: ABCDEFG
+//? Lowercase: abcdefg
+
+const subject = "Chemistry";
+const book = "chemistry";
+
+// if (subject.toLowerCase() === book.toLowerCase()) {
+//   console.log("I am reading book");
+// } else {
+//   console.log("I am wasting my time")
+// }
+
+
+
+const drink = " water";
+const liquid = "    water    "
+
+if (drink.trim() === liquid.trim()) {
+  console.log("Panir opor name life");
+} else {
+  console.log("Somudre pani ase khaite pari na");
+}
+
