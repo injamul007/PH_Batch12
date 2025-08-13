f07fbd999dc1339b114a75461c5600911daf6ea0
injamul007 <injamul007hoque@gmail.com>
2025-08-13 13:48:11 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/keys.js
@@ -0,0 +1,13 @@
+//?
+
+const computer = {
+  brand: "lenovo",
+  price: 35000,
+  processor: "intel",
+  hdd: "512gb",
+  ram: "16gb",
+  monitor: "HP",
+}
+
+const keys = Object.keys(computer); //? Access all the property of an Object[Keys]
+console.log(keys);
