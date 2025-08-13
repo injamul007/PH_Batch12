8bc9517976990f5c0d0535d574ef3bdd24251d0b
injamul007 <injamul007hoque@gmail.com>
2025-08-13 13:48:59 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/object.js
@@ -0,0 +1,31 @@
+//? Objects in JS
+
+const age = 21;
+
+const school = 'rifle square';
+
+const isPassed = true;
+
+const subjects = ['Bangla', 'English', 'Physics', 'Math']
+
+
+const bottle = {
+  brand: "Apple",
+  price: 45,
+  color: "white",
+  isClean: false,
+}
+
+
+//? Object is Non - Primitive type
+
+const subject = {
+  name: "biology",
+  teacher: "rasheda mam",
+  examDate: "29 feb",
+  chapters: ["first", "second", "third"],
+  exams: {
+    name: "Final Exam",
+    marks: 100,
+  }
+}
