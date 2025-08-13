b60708f1eadd8ba57796fc85f71594f188e229fc
injamul007 <injamul007hoque@gmail.com>
2025-08-13 13:48:41 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/nested.js
@@ -0,0 +1,24 @@
+//?
+
+const college = {
+  name: "vnc",
+  class: ['11', '12'],
+  events: ['science fair', 'victory day', '21 feb'],
+  unique: {
+    color: 'blue',
+    result: {
+      gpa: 5,
+      merit: "top",
+    }
+  }
+}
+
+// console.log(college.unique.result.gpa);
+
+// college.events[1] = 'hello';
+// college.events = 'hello';
+// console.log(college.events);
+// console.log(college);
+// delete college.class;
+
+console.log(college);
