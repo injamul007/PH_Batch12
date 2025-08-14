25c4c5da53fe4b030427e69bba567e8bdb0a7730
injamul007 <injamul007hoque@gmail.com>
2025-08-14 08:05:53 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/property.js
@@ -0,0 +1,18 @@
+//?
+
+const person = {
+  name: "sodor uddin",
+  age: 25,
+  profession: "developer",
+  salary: 25000,
+  isMarried: true,
+  'fav-place': ['bandarban', 'saintmartin', 'kuakata'],
+}
+
+//? dot notation .
+//? Bracket notation []
+// console.log(person.profession);
+// console.log(person["profession"]);
+// const income = person.salary;
+// console.log(income);
+console.log(person['fav-place']);
