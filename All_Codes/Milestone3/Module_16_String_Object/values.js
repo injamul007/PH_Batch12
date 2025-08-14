2b27636b15e6bb8bc642d5ef9e4abe52ef0f9ec9
injamul007 <injamul007hoque@gmail.com>
2025-08-14 08:07:13 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/values.js
@@ -0,0 +1,12 @@
+const computer = {
+  brand: "lenovo",
+  price: 35000,
+  processor: "intel",
+  hdd: "512gb",
+  ram: "16gb",
+  monitor: "HP",
+}
+
+const values = Object.values(computer);
+
+console.log(values);
