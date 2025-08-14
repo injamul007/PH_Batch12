7a3b4c719993ba5d71a8e1b702adb3fc4639c116
injamul007 <injamul007hoque@gmail.com>
2025-08-14 08:06:24 +0600

    first commit

---
+++ All_Codes/Milestone3/Module_16_String_Object/slice.js
@@ -0,0 +1,19 @@
+//? 
+
+const address = "AndorKilla";
+
+// console.log(address.slice(0,5))
+// console.log(address.slice(2,5))
+
+
+const sentence = "I am a good and hardworking person";
+
+// console.log(sentence.split(" "));
+
+const friends = "Rahim,kahim,dahim,lahim,fahim,sahim";
+
+// console.log(friends.split(","));
+
+const realFriends = [ 'Rahim', 'kahim', 'dahim', 'lahim', 'fahim', 'sahim' ];
+
+// console.log(realFriends.join("-"));
