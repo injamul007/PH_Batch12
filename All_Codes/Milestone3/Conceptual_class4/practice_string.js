9f7ff8db0611f5ec4af5bad41ec45e302577b143
injamul007 <injamul007hoque@gmail.com>
2025-08-15 15:04:44 +0600

    first commit

---
+++ All_Codes/Milestone3/Conceptual_class4/practice_string.js
@@ -0,0 +1,19 @@
+//?
+
+
+var message = "hello"
+
+var firstCh = message[0].toUpperCase();
+
+var restCh = message.slice(1);
+
+// console.log(firstCh.concat(restCh));
+
+
+// var text = "Javascript is awesome";
+// var temp = '';
+
+// for (let i = 0; i < text.length; i++) {
+//   temp = text[i] + temp;
+// }
+// console.log(temp);
