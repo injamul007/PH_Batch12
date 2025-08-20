68b1489a4063bc8cbf74234f9775fdcb26d50a79
injamul007 <injamul007hoque@gmail.com>
2025-08-20 21:28:22 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_18_more_array/recap.js
@@ -0,0 +1,26 @@
+//?
+
+//? Only Recap
+
+const books = ["physics", "javascript", "css", "math"]
+
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i])
+}
+
+const rev_books = books.reverse();
+
+const sorted_books = books.sort();
+
+const gadgets = [
+  {name: "Laptop", price: 35000, brand: "Hp"},
+  {name: "Watch", price: 12000, brand: "Samsung"},
+  {name: "Phone", price: 21000, brand: "samsung"}
+]
+
+//? 2D Array
+const matrix = [
+  [2, 88, 12],
+  [5, 7, 0],
+  [12, 9, 18]
+]
