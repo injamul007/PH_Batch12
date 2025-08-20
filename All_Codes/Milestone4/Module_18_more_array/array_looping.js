6af23ed00e45019e1177e706194adbc909322676
injamul007 <injamul007hoque@gmail.com>
2025-08-20 21:01:46 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_18_more_array/array_looping.js
@@ -0,0 +1,14 @@
+//?
+
+const fruits = ["Apple", "Banana", "Anar", "Watermelon", "Daav"]
+
+for (let i = 0; i < fruits.length; i++) {
+  // console.log(i, fruits[i]);
+}
+
+const numbers = [12, 45, 78, 145, 7, 44, 765]
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
