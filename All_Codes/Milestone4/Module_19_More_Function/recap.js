5abe23b8e7c9a581feea59b6cc2cbae658ec5a64
injamul007 <injamul007hoque@gmail.com>
2025-08-22 00:13:51 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/recap.js
@@ -0,0 +1,23 @@
+//? Recap all of the chapters of js
+
+function addTwoNum(price1, price2) {
+  const total = price1 + price2;
+  return total;
+}
+
+const bill = addTwoNum(15, 80);
+
+// console.log(bill)
+
+function doMath(num1, num2) {
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+  const multiply = sum * diff;
+  const result = multiply / 2;
+  return result;
+}
+
+const doMathResult = doMath(10, 5)
+// console.log(doMathResult);
+
+
