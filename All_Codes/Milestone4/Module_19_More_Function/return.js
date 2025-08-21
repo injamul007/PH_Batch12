aaf15794ef8c8c9d7264de734700d9e2a15dd063
injamul007 <injamul007hoque@gmail.com>
2025-08-22 00:14:05 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/return.js
@@ -0,0 +1,17 @@
+//? Return statement in function in js
+
+function tenTimes(num) {
+  const result = num * 10;
+  return result;
+} 
+
+const tenTimesResult = tenTimes(8);
+console.log(tenTimesResult);
+
+function cutHalf(num) {
+  const half = num / 2;
+  return half;
+}
+
+const cutHalfResult = cutHalf(8);
+console.log(cutHalfResult)
