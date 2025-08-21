6ab31e3d91da80356e662adb815ba5c6b9cd3b69
injamul007 <injamul007hoque@gmail.com>
2025-08-21 22:26:28 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/conditional_return.js
@@ -0,0 +1,23 @@
+//?
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// const even = isEven(33);
+// console.log(even);
+
+
+function isOdd(number) {
+  if(number % 2 !== 0) {
+    return true;
+  }
+  return false;
+}
+
+// console.log(isOdd(33));
+console.log(isOdd(34));
