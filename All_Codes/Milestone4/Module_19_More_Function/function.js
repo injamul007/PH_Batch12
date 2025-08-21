6fe402bb7edb715c5f01e2ff6ce49359fbb9ff92
injamul007 <injamul007hoque@gmail.com>
2025-08-21 22:27:14 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/function.js
@@ -0,0 +1,17 @@
+//? Function
+
+const price = 45;
+
+function fanOffKor() {
+  console.log("Bosa theke uthe dara")
+  console.log("Walk towards the switch")
+  console.log("click the switch to off of the Fan")
+}
+
+function brushYourTeeth() {
+  console.log("Pick up the brush")
+  console.log("Add some paste to your brush")
+  console.log("2 min goso and goso and then fena felo")
+}
+
+brushYourTeeth();
