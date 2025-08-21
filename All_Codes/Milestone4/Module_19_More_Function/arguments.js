63176fc5963f5f686d23663e05c32a13e938a33e
injamul007 <injamul007hoque@gmail.com>
2025-08-21 22:25:45 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/arguments.js
@@ -0,0 +1,23 @@
+//?
+
+function doubleIt(num) {
+  const double = num * 2
+  console.log(double)
+}
+
+// console.log("I will call the function");
+// doubleIt(88);
+// doubleIt(30);
+// doubleIt(100);
+// doubleIt(500);
+const money = 1120;
+// doubleIt(money);
+
+function difference(num1, num2) {
+  const diff = num1 - num2;
+  console.log(num1, num2, "Difference is: ", diff);
+}
+
+const fatherAge = 60;
+const myAge = 30;
+difference(fatherAge, myAge);
