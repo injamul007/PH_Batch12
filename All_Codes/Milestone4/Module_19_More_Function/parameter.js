2da50c7c7c74d572963a2a43837ed284f81317f8
injamul007 <injamul007hoque@gmail.com>
2025-08-22 00:13:17 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_19_More_Function/parameter.js
@@ -0,0 +1,41 @@
+//? Parameter in Function in js
+
+function vaatKhao() {
+  console.log("hand wash kore aso")
+  console.log("boso")
+  console.log("plate a khabar dabar nao")
+  console.log("gopat gopat kore khao")
+}
+
+// vaatKhao();
+
+
+function square(num) {
+  // console.log(num)
+  const borgo = num * num;
+  console.log("Square of the given number is: ", borgo);
+  // return num * num;
+}
+
+// square(11);
+// square(12);
+// square(13);
+// console.log(square(11));
+
+
+function add(num1, num2) {
+  const sum = num1 + num2;
+  console.log("The Sum of two number is: ", sum);
+}
+
+// add(120,30);
+// add(120,130);
+
+
+
+function addAll(a,b,c,d,e) {
+  const total = a+b+c+d+e
+  console.log(total);
+}
+
+// addAll(2,3,4,5,6);
