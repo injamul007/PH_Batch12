ed9fc626bca19f8ea83e6cda8951a4f0690ae20b
injamul007 <injamul007hoque@gmail.com>
2025-08-23 07:35:24 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_20_JS_coding_problem_1/date.js
@@ -0,0 +1,15 @@
+//? JS Date and Time
+
+// const date = new Date().getFullYear()
+
+// console.log(new Date('2025-08-13'));
+// console.log(new Date(2025,8,13));
+// console.log(new Date().toLocaleString());
+// console.log(new Date().toLocaleDateString());
+// console.log(new Date().toLocaleTimeString());
+// console.log(new Date().getFullYear())
+// console.log(new Date().getHours())
+// console.log(new Date().getMonth())
+
+// console.log(new Date().getMonth()+1);
+// console.log(new Date().getDay());
