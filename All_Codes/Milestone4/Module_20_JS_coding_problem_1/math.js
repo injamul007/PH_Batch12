92fe338e290101fa8c9bad39a75f2d89b1860198
injamul007 <injamul007hoque@gmail.com>
2025-08-24 20:32:02 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_20_JS_coding_problem_1/math.js
@@ -0,0 +1,32 @@
+//? All the Math method in js 
+
+// const result = Math.floor(Math.random() * 10) + 10
+// console.log(typeof Math.random())
+
+const min = Math.min(45,21,65,99,126,5,-99)
+// console.log(min)
+const max = Math.max(45,21,65,99,126);
+// console.log(max)
+
+// console.log(Math.abs(10-5)) 
+// console.log(Math.abs(5-10))
+// console.log(Math.abs(10-18))
+// console.log(Math.abs(18-10))
+// console.log(Math.abs(-18-10)) //? this will give difference !!
+
+// console.log(Math.floor(4.4))
+// console.log(Math.floor(4.1))
+// console.log(Math.floor(4.7)) //? this will give lowest value
+
+// console.log(Math.ceil(4.4))
+// console.log(Math.ceil(4.1))
+// console.log(Math.ceil(4.7)) //? this will give highest value
+
+
+// console.log(Math.round(4.1)) //? this will give lowest when value is below .4
+// console.log(Math.round(4.4))
+// console.log(Math.round(4.5))
+// console.log(Math.round(4.7)) //? this will give highest when value is above .5
+
+
+// console.log(Math.random());
