34687b9c90523e96ee098a7ada3437f0bf4187b9
injamul007 <injamul007hoque@gmail.com>
2025-08-28 15:45:33 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_21_JS_coding_problem_2/discount.js
@@ -0,0 +1,18 @@
+//?  Discount 
+
+function discountPrice(quantity) {
+  if (quantity <= 100) {
+    const total = quantity * 100;
+    return total;
+  } else if (quantity <= 200) {
+    const total = quantity * 90;
+    return total;
+  } else {
+    const total = quantity * 70;
+    return total;
+  }
+}
+
+console.log(discountPrice(4))
+console.log(discountPrice(7))
+console.log(discountPrice(191))
