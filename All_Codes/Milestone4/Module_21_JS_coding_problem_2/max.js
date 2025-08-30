aee6240008c2ac76e86697d4c8234d6ff761466e
injamul007 <injamul007hoque@gmail.com>
2025-08-30 20:16:51 +0600

    first commit

---
+++ All_Codes/Milestone4/Module_21_JS_coding_problem_2/max.js
@@ -0,0 +1,18 @@
+//? max in js 
+
+// if (disha > salman) {
+  // console.log("Disha will get the strawberry")
+// } else {
+  // console.log("salman will get the strawberry")
+// }
+
+//? inside a function
+
+function getMax(num1, num2) {
+  if (num1 > num2) {
+    return num1 + ' is number1 and it is max';
+  }
+  return num2 + ' is number2 and it is max';
+}
+
+console.log(getMax(96,87));
