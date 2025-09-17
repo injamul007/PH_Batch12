9dbe5a50183359283f4f15237a7b0ffc9f56fd76
injamul007 <injamul007hoque@gmail.com>
2025-09-17 14:08:45 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_29_Basic_ES6/template_string.js
@@ -0,0 +1,17 @@
+//?
+
+// const poem = 'amm pata jora jora'  //? cannot break the line
+
+const poem = `amm pata jora jora 
+marbo chabuk chorbo ghora`;
+
+// console.log(poem)
+
+function sum(num1, num2) {
+  const result = num1 + num2;
+  // const output = 'sum of ' + num1 + ' and ' + num2+ ' is equal to ' + result;
+  const output = `sum of ${num1*6} and ${num2} is equal to ${result}`
+  console.log(output)
+}
+
+// sum(10,20)
