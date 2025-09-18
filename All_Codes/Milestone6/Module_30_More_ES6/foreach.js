3068e6ebd415279c98f90ee8cfd1f6706cb14268
injamul007 <injamul007hoque@gmail.com>
2025-09-18 11:48:27 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_30_More_ES6/foreach.js
@@ -0,0 +1,9 @@
+//? foreach in js
+
+const numbers = [2,3,7,8]
+// const newNumbers = numbers.forEach(num => {
+//   return num*num
+// })
+// console.log(newNumbers)
+
+numbers.forEach(num => console.log(num*2))
