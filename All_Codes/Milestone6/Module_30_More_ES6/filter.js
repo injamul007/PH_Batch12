ccb84b23a642169ae3d5bd2b1fc4127d3c2ad342
injamul007 <injamul007hoque@gmail.com>
2025-09-18 11:48:04 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_30_More_ES6/filter.js
@@ -0,0 +1,8 @@
+//? filter in js
+
+const numbers = [1,5,6,1,45,27]
+
+const greaterThan10 = numbers.filter(num => num>10);
+// const greaterThan10 = numbers.filter(num => num%2!==0);
+
+console.log(greaterThan10)
