9f0a82dfb5a9760c05871106de148ad779ca00a2
injamul007 <injamul007hoque@gmail.com>
2025-09-30 16:51:51 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_31_Core_Concept_JS/data_type.js
@@ -0,0 +1,17 @@
+//? All Data Types of js
+
+//?  js primitive data type
+const price = 50;
+const name = 'Boss';
+const isPoor = false;
+let isKnown = undefined; // usually you should not set value to undefined;
+let isLocal;
+const merit = null;
+
+//? Non-Primitive data type
+
+const student = {name: 'boss', location: 'dhaka',}
+
+const friends = ['tom','bob','alice','jack']
+
+function sayName() {}
