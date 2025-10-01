fb2f3ef8378fea38ad72747c66d458b7c3483907
injamul007 <injamul007hoque@gmail.com>
2025-10-01 21:03:20 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_31_Core_Concept_JS/tripple_equal.js
@@ -0,0 +1,12 @@
+//? Triple EquaL Sign ===
+//? First check the types of data. if type is different , they immediately return false
+//? if the type is same then checks the value, if value and type is same then return true , if one of them is not matched exactly this will give false as result
+
+console.log(2 === 2)
+console.log(2 === '2')
+console.log(2 === 1)
+console.log(2 === true)
+console.log(false === 0)
+console.log(null === undefined)
+console.log(0 === '')
+// console.log([] === '')
