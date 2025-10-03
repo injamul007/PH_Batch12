0be038345ea79df9807caee3385280c77a46eaaa
injamul007 <injamul007hoque@gmail.com>
2025-10-04 02:45:41 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_34_5_More_Advance_JS/event_loop.js
@@ -0,0 +1,24 @@
+//? Event Loop in js 
+
+function one() {
+  two()
+  console.log(1)
+}
+function two() {
+  three()
+  console.log(2)
+}
+function three() {
+  setTimeout(()=> {console.log('inside of three')},0)
+  four()
+  console.log(3)
+}
+function four() {
+  five()
+  console.log(4)
+}
+function five() {
+  console.log(5)
+}
+
+one()
