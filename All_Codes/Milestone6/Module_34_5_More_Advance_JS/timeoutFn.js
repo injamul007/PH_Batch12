b1be14c5d0b3015adcd530302d095dc1b52ff533
injamul007 <injamul007hoque@gmail.com>
2025-10-05 06:43:32 +0600

    first commit

---
+++ All_Codes/Milestone6/Module_34_5_More_Advance_JS/timeoutFn.js
@@ -0,0 +1,21 @@
+//? setTimeOut and setTimeInterval
+
+console.log(1)
+console.log(2)
+// const timeOutId = setTimeout(()=> {console.log(3)},3000);
+console.log(4)
+console.log(5)
+
+// const intervalId = setInterval(() => {console.log(3)},3000)
+
+// let count = 0;
+// const intervalId = setInterval(() => {
+//     console.log(count++);
+//     if (count > 10) {
+//       clearInterval(intervalId)
+//     }
+// },3000)
+
+// clearTimeout(timeOutId)
+// clearInterval(intervalId)
+
