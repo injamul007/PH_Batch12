aa18e59b62baec4f7d49cebbd78f82706dbddec5
injamul007 <injamul007hoque@gmail.com>
2026-03-02 21:39:50 +0600

    update and add arrays file in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/arraysJs/arrays.js
@@ -0,0 +1,15 @@
+//? Arrays
+//? Arrays is a special type of object
+//? Arrays are mutable.
+
+// let arr = new Array(3).fill(0);
+
+// console.log(arr)
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]*2);
+}
