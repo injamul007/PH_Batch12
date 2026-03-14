3070d70f3eb98112110d5fc57cf9d522d08d63f0
injamul007 <injamul007hoque@gmail.com>
2026-03-14 20:07:44 +0600

    update and add array in it

--- All_Codes/Milestone3/Module_18_JS_recap/arraysJs/arrays.js
+++ All_Codes/Milestone3/Module_18_JS_recap/arraysJs/arrays.js
@@ -14,3 +14,56 @@
 //   console.log(arr[i]*2);
 // }
 
+// let arr = [2,4,6,8]
+
+// console.log(arr)
+
+
+// let arr = new Array("hello", 2, true)
+
+// console.log(arr)
+
+// let arr = [20]
+// let carr = new Array(20).fill(1)
+
+// console.log(arr)
+// console.log(carr)
+
+// let arr = [6,36,3,66,3]
+
+// for(let i = 0; i<arr.length; i++) {
+//   console.log(arr[i]*2)
+// }
+
+// for(let elem of arr) {
+//   console.log(elem)
+// }
+
+// for(let elem in arr) {
+//   console.log(elem, arr[elem])
+// }
+
+// arr[0] = 99
+
+// console.log(arr[0])
+// console.log(arr)
+
+
+// let arr = [1,2,3,4,5,6]
+
+// let newArr = arr.slice(0,4)
+// console.log(newArr)
+// console.log(arr)
+
+// let arr = [1,2,3,"boss",6]
+
+// arr.splice(3,1,"toss", 5,4)
+
+// console.log(arr)
+
+// let arr = [1,2,3,7,8,9]
+
+// let test = arr.splice(3,2,4,5,6)
+// console.log(arr)
+// console.log(test)
+
