3cbeb3a3163026b4db43c2e261c0119fdfaa3d3b
injamul007 <injamul007hoque@gmail.com>
2026-03-16 22:33:58 +0600

    update and add object in it

--- All_Codes/Milestone3/Module_18_JS_recap/objectjs/object.js
+++ All_Codes/Milestone3/Module_18_JS_recap/objectjs/object.js
@@ -1,16 +1,77 @@
-const car = {
-  brand: "Toyota",
-  color: "Red",
-  speed: `180kmh`,
-  drive: function() {
-    console.log(`The brand new ${this.brand} Car is Starting and it's color is ${this.color}`)
-  }
-};
+// const car = {
+//   "full name": "Boss",
+//   brand: "Toyota",
+//   "color": "Red",
+//   speed: `180kmh`,
+//   start: function() {
+//     return `Starting the ${this} car`
+//   },
+//   stop: ()=> {
+//     return `Stopping the ${this.brand} car`
+//   }
+// };
 
-// console.log(car.drive())
+// console.log(typeof car === "object")
+// console.log(!Array.isArray(car))
 
-const person = new Object();
-person.name = "boss"
-person.age = 32
+// const {brand, speed: topSpeed} = car
+// console.log(brand,topSpeed)
 
-console.log(person)
+// const test = 'full name'
+
+// console.log(car.hasOwnProperty('speed'))
+
+// Object.freeze(car)
+// car.brand = 'BMW'
+// delete car.color
+// car.origin = "Japan"
+// console.log(car)
+// Object.seal(car)
+// car.new = 'new'
+// delete car["full name"]
+// car["full name"] = 'toss'
+// console.log(car)
+
+// console.log(car)
+// const car2 = {...car}
+// console.log(car2)
+// const car2 = {}
+// Object.assign(car2, car)
+// console.log(car2)
+
+// console.log(car.speed)
+// console.log(car['color'])
+// console.log(car.start())
+// console.log(car.stop())
+// console.log(car[test])
+// console.log(this)
+// const keys = Object.keys(car)
+// const values = Object.values(car)
+// const entries = Object.entries(car)
+// console.log(keys)
+// console.log(values)
+// console.log(entries)
+
+// for(let elem of entries[1]) {
+//   console.log(elem)
+// }
+
+// for(let test in car) {
+//   console.log(test,car[test])
+// }
+
+// const person = new Object({
+//   test: "pass"
+// });
+// person.name = "boss"
+// person.age = 32
+
+// console.log(person)
+// person.name = "toss"
+// person.city = 'dhaka'
+// console.log(person)
+// delete person.city
+// console.log(person)
+
+
+
