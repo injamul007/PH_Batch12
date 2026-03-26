6773b48e74c06d45d85e8651e5ae263923e4feba
injamul007 <injamul007hoque@gmail.com>
2026-03-26 22:52:49 +0600

    update and add dom in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/DOM/js/Event.js
@@ -0,0 +1,81 @@
+// const firstBtn = document.querySelector('.firstBtn')
+
+// console.log(firstBtn)
+
+//? property method event listener
+// firstBtn.onclick = ()=> {console.log('btn clicked')}
+
+// firstBtn.onclick = ()=> {console.log('btn clicked twice')}
+
+//? Add Event Listener Method
+
+// firstBtn.addEventListener("click", ()=> {
+//   console.log('added event listener')
+// })
+
+// const showConsole = () => {
+//   console.log('this is event listener')
+// }
+
+// firstBtn.addEventListener("click", showConsole)
+
+// firstBtn.removeEventListener('click', showConsole)
+
+
+// const plusBtn = document.querySelector('.plusBtn')
+// const minusBtn = document.querySelector('.minusBtn')
+// const displayCount = document.querySelector('.displayCount')
+
+// displayCount.addEventListener('mouseenter', (event) => {
+//   console.log('mouse pointer just enter in the box', event)
+// })
+
+// displayCount.addEventListener('mouseleave', (event) => {
+//   console.log('mouse pointer just leave from the box', event)
+// })
+
+// displayCount.addEventListener('mouseover', (event) => {
+//   console.log('mouse pointer just leave from the box', event)
+// })
+
+// let count = displayCount.innerHTML
+
+// plusBtn.addEventListener("click", () => {
+//   displayCount.innerHTML = count++
+// })
+
+// minusBtn.addEventListener("click", () => {
+//   displayCount.innerHTML = count--
+// })
+
+
+// const input = document.querySelector('.input')
+
+// input.addEventListener("input", () => {
+//   console.log(input.value)
+// })
+
+// input.addEventListener("change", () => {
+//   console.log(input.value)
+// })
+
+
+// window.addEventListener('load', () => {
+//   console.log('loaded the window')
+// })
+
+// window.addEventListener('resize', () => {
+//   console.log('window is resized')
+// })
+
+// window.addEventListener('scroll', () => {
+//   console.log('window is scrolling')
+// })
+
+// const h2 = document.querySelector('h2')
+
+// h2.addEventListener('copy', () => {
+//   console.log(window.getSelection().toString())
+// })
+
+
