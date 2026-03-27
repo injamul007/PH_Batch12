b77aa6e659a1e1e7d6b15d0498b2192e31850f44
injamul007 <injamul007hoque@gmail.com>
2026-03-27 23:11:00 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/color-switcher/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/color-switcher/script.js
@@ -1,4 +1,7 @@
 const body = document.querySelector('body')
-const body = document.querySelector('body')
-const body = document.querySelector('body')
-const body = document.querySelector('body')
+const applyClrBtn = document.querySelector('.apply_clr_btn')
+const colorInputField = document.querySelector('.color_input_field')
+const randomClrBtn = document.querySelector('.random_clr_btn')
+const secondHeading = document.querySelector('.second_heading')
+
+
