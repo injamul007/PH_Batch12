0048634065a810a1700d42e2501cf9675525acfa
injamul007 <injamul007hoque@gmail.com>
2026-03-28 01:00:27 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/color-switcher/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/color-switcher/script.js
@@ -2,13 +2,16 @@ const body = document.querySelector("body");
 const applyClrBtn = document.querySelector(".apply_clr_btn");
 const colorInputField = document.querySelector(".color_input_field");
 const randomClrBtn = document.querySelector(".random_clr_btn");
-const secondHeading = document.querySelector(".second_heading");
+const colorValueField = document.querySelector(".color_value_field");
 
 
 
 applyClrBtn.addEventListener("click", () => {
-  body.style.backgroundColor = `${colorInputField.value}`
-  secondHeading.innerText = `Current Color: ${colorInputField.value}`
+  body.style.backgroundColor = colorInputField.value;
+  colorValueField.innerText = colorInputField.value;
 });
 
 
+randomClrBtn.addEventListener('click', ()=> {
+  
+})
