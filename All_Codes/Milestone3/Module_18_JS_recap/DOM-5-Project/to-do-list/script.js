5640c3075c66ddd43ce239f8091e71133d082758
injamul007 <injamul007hoque@gmail.com>
2026-04-01 08:33:40 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/to-do-list/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/DOM-5-Project/to-do-list/script.js
@@ -1,30 +1,23 @@
 const listContainer = document.querySelector(".listContainer");
 const inputTaskField = document.querySelector('#taskAddInput')
 const form = document.querySelector("form");
-const taskDeleteBtn = document.querySelector(".taskDeleteBtn");
 
-const todoListArr = [
-  // {
-  //   id: 1,
-  //   task: "i will become a good full-stack developer",
-  // },
-  // {
-  //   id: 2,
-  //   task: "and for that i have to be a discipline person",
-  // },
-  // {
-  //   id: 3,
-  //   task: "that mean i have to leave my comfort zone",
-  // },
-];
+let todoListArr = [];
 
 function todoRenderList(arr) {
   listContainer.innerHTML = ''
   arr.map((item) => {
     const divElem = document.createElement("div");
     divElem.classList.add("listItem");
-    divElem.innerHTML = `<p>${item.task}</p>
-        <button class="taskDeleteBtn">❌</button>`;
+    divElem.innerHTML = `<p>${item.task}</p>`;
+    
+    const buttonElem = document.createElement('button')
+    buttonElem.classList.add('taskDeleteBtn')
+    buttonElem.innerText = '❌'
+    divElem.append(buttonElem)
+    buttonElem.addEventListener('click', ()=> {
+      handleTaskDelete(item.id)
+    })
     listContainer.append(divElem);
   });
 }
@@ -32,17 +25,24 @@ function todoRenderList(arr) {
 todoRenderList(todoListArr)
 
 const handleSubmitTask = (e)=> {
-  e.preventDefault(); 
+  e.preventDefault();
   if(!inputTaskField.value) {
     console.log('please add something in input field')
     return null
   }
   const taskInputValue = inputTaskField.value;
+  inputTaskField.value = ''
   const taskAddObj = {
-    id: 1,
+    id: Date.now(),
     task: taskInputValue
   }
   todoListArr.push(taskAddObj)
+  todoRenderList(todoListArr)
+}
+
+function handleTaskDelete(id) {
+  const filteredDelete = todoListArr.filter(item => item.id !== id)
+  todoListArr = filteredDelete
   todoRenderList(todoListArr)
 }
 
