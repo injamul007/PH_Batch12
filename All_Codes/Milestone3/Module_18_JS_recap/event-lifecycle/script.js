f6da5e8276bb1f0511bc09a86c01e417aa999015
injamul007 <injamul007hoque@gmail.com>
2026-04-07 20:19:41 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/event-lifecycle/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/event-lifecycle/script.js
@@ -5,12 +5,12 @@ const child = document.querySelector('.child')
 
 grandParent.addEventListener('click', ()=>{
   console.log('grand parent click is working fine')
-}, true)
+})
 
 parent.addEventListener('click', ()=>{
   console.log('parent click is working fine')
-}, true)
+})
 
 child.addEventListener('click', ()=>{
   console.log('child click is working fine')
-}, true)
+})
