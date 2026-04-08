ce089f135e515661587c1a95f17f229813899be3
injamul007 <injamul007hoque@gmail.com>
2026-04-08 12:19:16 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/event-lifecycle/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/event-lifecycle/script.js
@@ -3,14 +3,47 @@ const parent = document.querySelector('.parent')
 const child = document.querySelector('.child')
 
 
+// grandParent.addEventListener('click', ()=>{
+//   console.log('grand parent click is working fine')
+// })
+
+// grandParent.addEventListener('click', ()=>{
+//   console.log('grand parent click is working fine')
+// }, true)
+
+// parent.addEventListener('click', ()=>{
+//   console.log('parent click is working fine')
+// })
+
+// parent.addEventListener('click', ()=>{
+//   console.log('parent click is working fine')
+// }, true)
+
+// child.addEventListener('click', ()=>{
+//   console.log('child click is working fine')
+// })
+
+// child.addEventListener('click', ()=>{
+//   console.log('child click is working fine')
+// }, true)
+
+
 grandParent.addEventListener('click', ()=>{
   console.log('grand parent click is working fine')
-})
+}, false)
+
+// parent.addEventListener('click', ()=>{
+//   console.log('parent click is working fine')
+// })
 
 parent.addEventListener('click', ()=>{
   console.log('parent click is working fine')
-})
+}, true)
+
+// child.addEventListener('click', ()=>{
+//   console.log('child click is working fine')
+// }, true)
 
 child.addEventListener('click', ()=>{
   console.log('child click is working fine')
-})
+}, true)
