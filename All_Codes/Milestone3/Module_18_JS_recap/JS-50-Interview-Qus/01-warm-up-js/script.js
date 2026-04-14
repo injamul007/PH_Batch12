c9b1fbb7b9daf50e2a43c24f0eb1dbf972f9d6a2
injamul007 <injamul007hoque@gmail.com>
2026-04-14 19:06:51 +0600

    update and add dom in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/JS-50-Interview-Qus/01-warm-up-js/script.js
@@ -0,0 +1,12 @@
+//? 50 interview question of JS
+
+
+//? Ques No 1. Create a function that returns the last element of an array
+
+// function getLastElement(arr) {
+//   return arr[arr.length-1]
+// }
+
+// console.log(getLastElement([2,3,4,99,22,66]))
+
+
