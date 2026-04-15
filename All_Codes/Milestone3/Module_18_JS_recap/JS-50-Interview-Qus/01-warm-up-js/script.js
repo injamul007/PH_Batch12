6e2fbb428838d0d27298d1c82ff5dbfa34339a95
injamul007 <injamul007hoque@gmail.com>
2026-04-15 17:08:58 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/JS-50-Interview-Qus/01-warm-up-js/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/JS-50-Interview-Qus/01-warm-up-js/script.js
@@ -10,3 +10,14 @@
 // console.log(getLastElement([2,3,4,99,22,66]))
 
 
+//? Ques No 2. Combination of two array
+
+const arr1 = [1,2,3]
+const arr2 = [5,6,7]
+
+// const combination = arr1.concat(arr2)
+// const combination = [...arr1,...arr2]
+// console.log(combination)
+
+
+//? Ques No 3. 
