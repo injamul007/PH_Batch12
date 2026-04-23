950382188cb3dcaa45fcf1a9a7fc89b7c0b9c8ac
injamul007 <injamul007hoque@gmail.com>
2026-04-23 22:10:17 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/JS-50-Interview-Qus/03-dom-ques-js/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/JS-50-Interview-Qus/03-dom-ques-js/script.js
@@ -2,3 +2,18 @@
 
 //?  Ques No 31: How can you update the DOM based on user input in real-time (e.g., live preview of a form)?
 
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const nameValue = document.querySelector('.nameValue')
+const emailValue = document.querySelector('.emailValue')
+
+const handleEvent = (e) => {
+  if(e.target.value === nameInput.value) {
+    nameValue.innerText = e.target.value
+  } else {
+    emailValue.innerText = e.target.value 
+  }
+}
+
+nameInput.addEventListener('input', handleEvent)
+emailInput.addEventListener('input', handleEvent)
