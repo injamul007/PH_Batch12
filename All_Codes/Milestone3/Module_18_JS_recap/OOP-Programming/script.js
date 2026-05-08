87f886c5aa1d2b5f3814f44d05a529cd6440c125
injamul007 <injamul007hoque@gmail.com>
2026-05-08 19:16:21 +0600

    update and and dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/OOP-Programming/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/OOP-Programming/script.js
@@ -1,2 +1,76 @@
 //? OOP Programming
 
+// const obj = {
+//   name: "boss",
+//   age: 32,
+//   passion: "coder",
+//   introduceMySelf: function(){
+//     console.log(this)
+//     console.log(`this is my name -> ${this.name}`)
+//     console.log(`this is my age -> ${this.age}`)
+//     console.log(`this is my passion -> ${this.passion}`)
+//   }
+// }
+
+// console.log(obj)
+// console.log(obj.introduceMySelf())
+
+
+//? Factory Function
+
+// function Student(name,age,passion) {
+//   return {
+//     name: name,
+//     age: age,
+//     passion: passion,
+//     introduceMySelf: function () {
+//       console.log(this);
+//       console.log(`this is my name -> ${this.name}`);
+//       console.log(`this is my age -> ${this.age}`);
+//       console.log(`this is my passion -> ${this.passion}`);
+//     },
+//   };
+// }
+
+// const result1 = Student('boss',32,'coding');
+// const result2 = Student('mira',30,'cooking');
+// console.log(result1);
+// console.log(result2);
+
+
+//? Constructor Function
+
+// function Student(name,age,passion) {
+//   this.name = name
+//   this.age = age
+//   this.passion = passion
+//   return this
+// }
+
+// const result = new Student('boss',32,'coding')
+// console.log(result)
+
+// const arr = new Array
+// console.log(typeof arr)
+// console.log(typeof Array)
+
+
+//? Class Constructor
+
+// class Car{
+//   constructor(brand,speed){
+//     this.brand = brand
+//     this.speed = speed
+//   }
+//   start(){
+//     return 'this car is starting'
+//   }
+//   stop(){
+//     return 'this car is stop'
+//   }
+// }
+
+// const BMW = new Car('BMW','200mph')
+// const Mercedes = new Car('Mercedes','220mph')
+// console.log(BMW)
+// console.log(Mercedes)
