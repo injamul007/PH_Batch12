f297171a9a4a973ef7a84f4c8d0a072b78d234bc
injamul007 <injamul007hoque@gmail.com>
2026-05-17 18:13:53 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/loops/loops.js
+++ All_Codes/Milestone3/Module_18_JS_recap/loops/loops.js
@@ -297,3 +297,13 @@
 // }
 
 // console.log(Object.entries(user))
+
+// let counter = 0
+// for (let i = 1; i < 101; i++) {
+//   if(i % 2 !== 0){
+//     counter++
+//     console.log(i)
+//   }
+  
+//   if(counter === 5) break;
+// }
