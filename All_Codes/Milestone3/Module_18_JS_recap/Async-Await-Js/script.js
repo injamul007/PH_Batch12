9809c0b9efab73b484bf15578a12237bb6bcab5a
injamul007 <injamul007hoque@gmail.com>
2026-05-18 21:20:11 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/script.js
@@ -31,3 +31,13 @@
 //?                         calculation             timers 
 
 
+// async function fetchData() {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//   const resultData = await response.json()
+//   console.log(resultData)
+// }
+
+// fetchData()
+
+// console.log('hello')
+// console.log('how are you?')
