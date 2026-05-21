41d6a260bdecfb44a4c8da13a6f9d552a6a69596
injamul007 <injamul007hoque@gmail.com>
2026-05-21 21:57:26 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
@@ -1,1 +1,8 @@
-// console.log('connecting')
+
+const taskInput = document.querySelector('#taskInput')
+const addBtn = document.querySelector('.addBtn')
+
+
+addBtn.addEventListener('click', ()=> {
+  const value = taskInput.value;
+})
