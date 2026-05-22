a9946f402372790627fa3ac314b80e786365a671
injamul007 <injamul007hoque@gmail.com>
2026-05-22 07:28:36 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
@@ -1,8 +1,32 @@
+const taskInput = document.querySelector("#taskInput");
+const addBtn = document.querySelector(".addBtn");
+const todoContainer = document.querySelector(".todoContainer");
 
-const taskInput = document.querySelector('#taskInput')
-const addBtn = document.querySelector('.addBtn')
+const API = "https://6a0f2cad1736097c360b3d4c.mockapi.io/api/v1/advanceTodos";
 
-
-addBtn.addEventListener('click', ()=> {
+addBtn.addEventListener("click", () => {
   const value = taskInput.value;
-})
+});
+
+async function fetchData(api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+
+    //? looping
+    data.forEach(elem => {
+      console.log(elem)
+      const todoDiv = document.createElement('div')
+      todoDiv.classList.add('todo')
+      todoDiv.innerHTML = `<p>${elem.text}</p>
+          <button>Edit</button>
+          <button>Delete</button>`
+
+      todoContainer.append(todoDiv)
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchData(API);
