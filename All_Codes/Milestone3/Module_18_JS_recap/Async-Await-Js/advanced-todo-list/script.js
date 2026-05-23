ab11dba82f2bfc3c300bd55f74537c406bb76368
injamul007 <injamul007hoque@gmail.com>
2026-05-23 14:36:21 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/advanced-todo-list/script.js
@@ -6,7 +6,6 @@ const API = "https://6a0f2cad1736097c360b3d4c.mockapi.io/api/v1/advanceTodos";
 
 
 addBtn.addEventListener("click", () => {
-  // const value = taskInput.value;
   postTodo()
 });
 
@@ -16,16 +15,52 @@ async function fetchTodo() {
     const data = await response.json();
     
     //? looping
-    todoContainer.innerHTML = ''
+    if(data) {
+      todoContainer.innerHTML = ''
     data.forEach(elem => {
       const todoDiv = document.createElement('div')
       todoDiv.classList.add('todo')
-      todoDiv.innerHTML = `<p>${elem.text}</p>
-      <button>Edit</button>
-      <button onclick="deleteTodo(${elem.id})">Delete</button>`
+      todoDiv.innerHTML = `
+      <input class="editInput" type="text"  value="${elem.text}" readonly />
+      <button class="editBtn">Edit</button>
+      <button class="saveBtn">Save</button>
+      <button class="deleteBtn">Delete</button>`
+
+      const editInput = todoDiv.querySelector('.editInput')
+      editInput.style.outline = 'none'
+      editInput.style.cursor = "default"
+      
+      const editBtn = todoDiv.querySelector('.editBtn')
+      editBtn.addEventListener('click', ()=> {
+        editBtn.style.display = 'none'
+        saveBtn.style.display = 'flex'
+        editInput.readOnly  = false
+        editInput.style.border = '1.5px solid black'
+        editInput.style.backgroundColor = 'white'
+        editInput.style.cursor = "text"
+      })
+      
+      const saveBtn = todoDiv.querySelector('.saveBtn')
+      saveBtn.style.display = 'none'
+      saveBtn.addEventListener('click',()=> {
+        editBtn.style.display = 'flex'
+        saveBtn.style.display = 'none'
+        editInput.readOnly  = true
+        editInput.style.border = 'none'
+        editInput.style.backgroundColor = '#16918c'
+        editInput.style.cursor = "default"
+
+        updateTodo(elem.id, editInput.value)
+      })
+
+      const deleteBtn = todoDiv.querySelector('.deleteBtn')
+      deleteBtn.addEventListener('click', ()=> {
+        deleteTodo(elem.id)
+      })
       
       todoContainer.append(todoDiv)
     });
+    }
   } catch (error) {
     console.log(error)
   }
@@ -54,12 +89,40 @@ async function postTodo() {
     if(response.status === 201) {
       fetchTodo()
     }
-    
+
     taskInput.value = ''
   } catch (error) {
     console.log(error)
   }
 }
+
+
+async function updateTodo(id,editValue){
+  try {
+    if(editValue === '') {
+      fetchTodo()
+      return
+    }
+
+    const response = await fetch(`${API}/${id}`, {
+      method: "PUT",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: editValue.trim()
+      })
+    })
+
+    if(response.status === 200){
+      fetchTodo()
+    }
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 
 async function deleteTodo(id) {
