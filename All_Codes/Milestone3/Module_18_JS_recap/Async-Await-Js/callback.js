b8f0677fc85bf6052229278d5fd014a9849f7394
injamul007 <injamul007hoque@gmail.com>
2026-05-26 09:56:35 +0600

    update and add dom in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/callback.js
@@ -0,0 +1,14 @@
+//? Callback is nothing but a function
+//? Callback is a function passed as an argument to another function
+
+// function hod(name, callback){
+//   callback(name)
+// }
+
+
+// function printName(name) {
+//   console.log('hello', name)
+// }
+
+
+// hod('mkl', printName)
