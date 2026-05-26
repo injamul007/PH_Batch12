5210dd3874722324842401039324aca5c3c1cecf
injamul007 <injamul007hoque@gmail.com>
2026-05-26 10:36:32 +0600

    update and add dom in it

--- All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/callback.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Async-Await-Js/callback.js
@@ -1,5 +1,6 @@
 //? Callback is nothing but a function
 //? Callback is a function passed as an argument to another function
+//? higher order function (hod) is a function that takes another function in it as an argument also call itself a callback function
 
 // function hod(name, callback){
 //   callback(name)
@@ -12,3 +13,22 @@
 
 
 // hod('mkl', printName)
+
+
+// function calculate(a,b, callback){
+//   callback(a,b)
+// }
+
+// function sum(a,b){
+//   console.log(a + b)
+// }
+// function subtract(a,b){
+//   console.log(a - b)
+// }
+// function multiply(a,b){
+//   console.log(a * b)
+// }
+
+// calculate(5,5, sum)
+// calculate(5,3, subtract)
+// calculate(5,5, multiply)
