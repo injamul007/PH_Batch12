c5adb84f9f5953717ef9bbffa200a003d9092783
injamul007 <injamul007hoque@gmail.com>
2026-06-01 14:20:13 +0600

    update and add local storage code in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/Local-and-session-storage/script.js
@@ -0,0 +1,21 @@
+
+
+// const form = document.querySelector('form')
+// const input = document.querySelector('input')
+
+// form.addEventListener('submit', (e)=>{
+//   e.preventDefault()
+  
+//   localStorage.setItem("Name", input.value)
+// })
+
+
+// console.log(localStorage.getItem('Name'))
+// localStorage.Name = 'kala tausi'
+// console.log(localStorage.setItem('Name', 'kala billi'))
+// localStorage.removeItem('naam')
+// localStorage.clear()
+
+
+
+
