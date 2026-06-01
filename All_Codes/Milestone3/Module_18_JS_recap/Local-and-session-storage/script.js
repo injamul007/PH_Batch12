a2515ad9c6028c08a5db2f6a4b5c6e169e055f4c
injamul007 <injamul007hoque@gmail.com>
2026-06-02 00:20:45 +0600

    update and add local storage in it

--- All_Codes/Milestone3/Module_18_JS_recap/Local-and-session-storage/script.js
+++ All_Codes/Milestone3/Module_18_JS_recap/Local-and-session-storage/script.js
@@ -17,5 +17,56 @@
 // localStorage.clear()
 
 
+// localStorage.setItem('name','boss')
+// console.log(localStorage.getItem('name'))
+// localStorage.removeItem('name')
+// console.log(localStorage.getItem('name'))
+// localStorage.clear()
 
 
+// sessionStorage.setItem('userId','1234')
+// console.log(sessionStorage.getItem('userId'))
+// sessionStorage.removeItem('userId')
+// console.log(sessionStorage.getItem('userId'))
+
+
+// localStorage.setItem('name','boss')
+// localStorage.setItem('age','32')
+
+// console.log(localStorage.key(0))
+// localStorage.clear()
+
+
+// const user = {
+//   name: 'boss',
+//   age: 32,
+//   isMarried: false,
+//   userCarCollection: ['bmw','audi','buggy',32]
+// }
+
+// console.log(user)
+
+// localStorage.setItem('userData', JSON.stringify(user))
+// localStorage.removeItem()
+// const rcv = JSON.parse(localStorage.getItem('userData'))
+// console.log(rcv.age)
+
+// localStorage.setItem('userName', JSON.stringify(user.name))
+// localStorage.setItem('userAge', JSON.stringify(user.age))
+// localStorage.setItem('isMarried', JSON.stringify(user.isMarried))
+// localStorage.setItem('userCarCollection', JSON.stringify(user.userCarCollection))
+// console.log(localStorage)
+
+// const isMarried = JSON.parse(localStorage.getItem('isMarried'))
+// const isMarried = localStorage.getItem('isMarried')
+// const userCollection = JSON.parse(localStorage.getItem('userCarCollection'))
+// const userCollection = localStorage.getItem('userCarCollection')
+
+// console.log(isMarried , "is now ----->", typeof isMarried)
+// console.log(userCollection , "is now ----->", typeof userCollection)
+
+// if(!isMarried){
+//   console.log('he is not married')
+// } else {
+//   console.log('he is out')
+// }
