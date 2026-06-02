ffbff13d7090a68193ec3798a9fa021bbf95de8b
injamul007 <injamul007hoque@gmail.com>
2026-06-03 00:02:44 +0600

    update and add destructuring related ques in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/another-4-qna/script.js
@@ -0,0 +1,43 @@
+//? Q&A NO 1: what will be the output?
+
+// const arr = [1,2,3]
+// const obj = {...arr}
+// console.log(obj)
+
+
+//? Q&A NO 2: how does using spread help avoid mutation modify the object without affecting the original ?
+
+// const original = {name: 'boss',age: 32}
+
+// const copyObj = {...original}
+// copyObj.company = 'levis'
+
+// console.log(original)
+// console.log(copyObj)
+
+
+//? Q&A NO 3: 
+
+// function separateEvenOdd(...numbers){
+//   const even = numbers.filter(item => {
+//     return item % 2 === 0
+//   })
+
+//   const odd = numbers.filter(item => {
+//     return item % 2 !== 0
+//   })
+
+//   return {
+//     even,
+//     odd
+//   }
+// }
+
+// const {even , odd} = separateEvenOdd(1,2,3,4,5,6)
+// console.log(even)
+// console.log(odd)
+
+
+
+//? Q&A NO 4:  React useState like functions making ....?!
+
