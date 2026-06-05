82aed73602fdcfddd51ffc9e4b27ef3e7a078222
injamul007 <injamul007hoque@gmail.com>
2026-06-05 18:32:01 +0600

    update and add module in it

---
+++ All_Codes/Milestone3/Module_18_JS_recap/Module-in-JS/math.js
@@ -0,0 +1,29 @@
+// let name = 'boss'
+// let age = 32
+
+// function sum(a,b) {
+//   return a+b
+// }
+
+// function subtract(a,b) {
+//   return a-b
+// }
+
+// export function multiply(a,b){
+//   return a * b
+// }
+
+// module.exports = {
+//   sum,
+//   subtract
+// }
+
+// export default {
+//   sum,
+//   subtract
+// }
+
+// export {
+//   sum,
+//   subtract
+// }
---
+++ All_Codes/Milestone3/Module_18_JS_recap/Module-in-JS/script.js
@@ -0,0 +1,17 @@
+
+// const obj = require('./math.js')
+// const {age} = obj
+// console.log(obj.name)
+// console.log(age)
+// const {sum,subtract} = require('./math.js')
+
+// const {sum} = obj
+// console.log(subtract(4,2))
+
+// import {sum,subtract,multiply} from './math.js'
+
+// const {sum,subtract} = obj
+
+// console.log(sum(2,7))
+// console.log(subtract(7,3))
+// console.log(multiply(7,3))
