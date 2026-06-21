f6dfcb2bcd758917eb82c29747a7249f66482986
injamul007 <injamul007hoque@gmail.com>
2026-06-21 12:04:24 +0600

    update and add if else in it

---
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/05_if_else_condition/script.js
@@ -0,0 +1,43 @@
+// const userAge = 19
+
+// if(userAge >= 18) {
+//   console.log('user can vote now');
+// } else {
+//   console.log('user cannot vote now');
+// }
+
+// console.log(+null);
+
+// console.log('24');
+// console.log(+'24');
+// console.log(+'24 k');
+// console.log(parseInt('24 k'));
+// console.log(parseInt('k 24 k'));
+// console.log(Number('k 24 k'));
+
+// const userName = prompt("Enter Your Name !") || "Mr. Guest"
+// const userAge = parseInt(prompt("Enter Your Age !") || 18)
+
+// console.log(`Name: ${userName}`)
+// console.log(`Age: ${userAge}`)
+
+// if (userAge >= 18 && userAge <= 24) {
+// 	console.log(`${userName} is a college student !`)
+// 	console.log(`${userName} is learning computer science`)
+// 	if (userAge == 20) {
+// 		console.log("this is his birth year")
+// 	}
+// } else if (userAge >= 25 && userAge <= 45) {
+// 	console.log(`${userName} is a working professional !`)
+// 	console.log(`${userName} is a web developer`)
+// 	if (userAge > 40) {
+// 		console.log("he is middle aged")
+// 	}
+// }
+
+
+// console.log(+false);
+// console.log(+null);
+// console.log(0 === 0);
+// console.log(false === null);
+// console.log(+false === +null);
