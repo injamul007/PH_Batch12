7a8d429c628dc1ac8bd004749482467b12fec3a4
injamul007 <injamul007hoque@gmail.com>
2026-06-23 07:06:15 +0600

    update and add dom in it

---
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/07_object_js/script.js
@@ -0,0 +1,27 @@
+// const user = { firstName: "boss" }
+
+// console.log(user)
+
+// const myName = 'boss'
+
+const user1 = {
+  firstName: 'toss',
+  lastName: 'bull',
+  'boss': 'Developer'
+}
+
+// user1 = 'hello'
+// user1.firstName = 'bulla'
+
+// delete user1.boss
+
+// Object.seal(user1)
+// Object.freeze(user1)
+
+// console.log("firstName" in user1);
+
+console.log(user1);
+// let userName = 'boss2'
+
+// console.log(user1[myName]);
+// console.log(user1['fist' + 'Name']);
