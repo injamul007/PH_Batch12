fc99de1d8187b93541040547e72a495725827a55
injamul007 <injamul007hoque@gmail.com>
2026-06-24 22:16:16 +0600

    update and add dom in it

---
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/08_arrays_js/script.js
@@ -0,0 +1,15 @@
+// const myName = 'boss'
+
+const clrArr = ['black','blue','red','green','yellow']
+
+// clrArr[clrArr.length-1] = 'lightGreen'
+// clrArr.push('lightBlue')
+
+// clrArr[10] = 'hello'
+// clrArr.push(myName)
+// clrArr.pop(clrArr[1]) //? in this method , anything you give in the as parameter wont work for this
+
+// clrArr.firstName = 'bigBoss'
+
+console.log(clrArr);
+// console.log(clrArr[5][1]);
