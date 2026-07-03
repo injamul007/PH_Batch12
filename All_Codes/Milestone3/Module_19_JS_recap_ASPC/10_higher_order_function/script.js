2a565220d22ada2bbb13f319271bd46e6d84c44e
injamul007 <injamul007hoque@gmail.com>
2026-07-03 23:48:43 +0600

    update and add higher order functions in it

--- All_Codes/Milestone3/Module_19_JS_recap_ASPC/10_higher_order_function/script.js
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/10_higher_order_function/script.js
@@ -1,7 +1,18 @@
+function a(b) {
+	// console.log(b)
 
-function a (b) {
-  console.log(b);
+	return b()
 }
 
 // console.log(a);
 // console.dir(a);
+// a.age = 30 //? functions is also a object behind the scene,, that's the proof
+
+// a("helloB")
+a(helloB)
+// console.log(result);
+
+
+function helloB () {
+	console.log("hello b is the function in that")
+}
