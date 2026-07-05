b14f75a5413ee2f8d998308b346385f9ea094f44
injamul007 <injamul007hoque@gmail.com>
2026-07-05 21:42:59 +0600

    update and add settimeout and setinterval in it

--- All_Codes/Milestone3/Module_19_JS_recap_ASPC/11_setTimeOut_and_setInterval/script.js
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/11_setTimeOut_and_setInterval/script.js
@@ -1,3 +1,14 @@
 
 
 // setTimeout('console.log(true)')
+
+
+// setTimeout('console.log("hello world-1")', 2000)
+// setTimeout('console.log("hello world-2")', 0)
+// setTimeout(a, 3000)
+
+// function a() {
+//   console.log('hello world-3');
+// }
+
+// console.log('no hello world');
