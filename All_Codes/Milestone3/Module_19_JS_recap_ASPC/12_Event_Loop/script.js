41bd9d7f706a427f348e328d0789bace9fffc4ef
injamul007 <injamul007hoque@gmail.com>
2026-07-10 08:07:45 +0600

    update and add event loop in it

--- All_Codes/Milestone3/Module_19_JS_recap_ASPC/12_Event_Loop/script.js
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/12_Event_Loop/script.js
@@ -1,1 +1,15 @@
-// console.log('event loop');
+// // console.log('event loop');
+
+var username = "Tom"
+
+// let tom  = ()=> {
+//   var username = 'Hi Tom'
+//   console.log(this.username + " runs");
+// }
+
+function tom() {
+	// var username = "Hi Tom"
+	console.log(this.username + " runs")
+}
+
+tom()
