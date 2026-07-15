f419ee5ef44e056a8e375e60c29f35a4fb548531
injamul007 <injamul007hoque@gmail.com>
2026-07-15 13:53:43 +0600

    update and add event loop in it

--- All_Codes/Milestone3/Module_19_JS_recap_ASPC/12_Event_Loop/script.js
+++ All_Codes/Milestone3/Module_19_JS_recap_ASPC/12_Event_Loop/script.js
@@ -13,3 +13,28 @@
 // }
 
 // tom()
+
+
+// console.log('inside global execution context');
+
+// var a = 5;
+
+// function testMe(){
+//   console.log('inside testMe execution context');
+//   var b = 10;
+//   var user = {
+//     name: 'boss',
+//     country: 'bd'
+//   }
+//   function testAgain(){
+//     console.log('inside testAgain execution context');
+//     console.log('Exiting testAgain execution context');
+//   }
+  
+//   testAgain()
+//   console.log('Exiting testMe execution context');
+// }
+
+// testMe();
+
+// console.log('Exiting global execution context');
