8f72d5c6deec84aa87034eacaae843cb2afc9d9b
injamul007 <injamul007hoque@gmail.com>
2026-07-30 19:14:44 +0600

    update and add python crash course in it

---
+++ All_Codes/Milestone3/python_crash_course/01_basics/code.py
@@ -0,0 +1,18 @@
+# name = 'boss is my name'
+# age = 32
+# print(type(name))
+
+
+
+# input('Enter your name: ')
+
+
+# age = input('Enter your age: ')
+
+# naw_age = int(age) + 1
+
+# print(naw_age)
+
+# print(float(naw_age))
+
+
