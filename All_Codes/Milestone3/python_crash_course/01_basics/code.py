61844a4dfbec248b92375c9e1258f7cdfc702ac1
injamul007 <injamul007hoque@gmail.com>
2026-08-08 06:40:34 +0600

    update and add python crash course in it

--- All_Codes/Milestone3/python_crash_course/01_basics/code.py
+++ All_Codes/Milestone3/python_crash_course/01_basics/code.py
@@ -139,3 +139,95 @@ name = 'Tony Stark'
 #? For Loops
 # for i in range(1,11):
 #   print(i)
+
+# for i in range(1,11):
+#   if i % 2 == 0:
+#     print(i)
+
+# for i in range(2, 11, 2):
+#   print(i)
+
+
+#? break & continue
+
+# for i in range(1, 31):
+#     if(i==21):
+#       continue
+#     if (i % 3 == 0):
+#       print(i)
+
+# print('out of loop')
+
+#? Practice Exercise 4:
+
+# print all odd number from 1 to 20
+
+# for i in range(1,20,2):
+#   print(i)
+
+
+# for i in range(57, 571):
+#     if (i % 57 == 0):
+#       print(i)
+
+# a = int(input("Enter any num a: "))
+# b = int(input("Enter any num b: "))
+
+# for i in range(1, 1001):
+#   if i % a == 0 and i % b == 0:
+#     print(i)
+#     break
+
+#? non-primitive data type or complex ones are:
+# List --- is an array but the name is changed here - and it's also mutable
+# Tuple
+# Set
+# Dictionary
+
+# marks = [98,99,100]
+
+# marks.append(7)
+# marks.insert(0,7)
+# marks.clear()
+# print(100 in marks)
+# print(len(marks))
+# print(marks[-1])
+# print(marks[0:2])
+
+# for score in marks:
+#   print(score)
+
+
+#? Tuple --- is also an array but the name is changed here - and it's immutable
+# marks = (98,99,100)
+# print(marks, type(marks))
+
+
+#? Set -- unique items collection
+# marks = {98,97,95,96}
+
+
+#? Dictionary -- key value pair -- in sort of object type
+
+# marks = {"math": 99, "physics": 98, "chemistry": 97}
+
+# marks['english'] = 94
+# print(marks)
+# print(marks["math"])
+
+
+# for key in marks:
+#   print(key,"--", marks[key])
+
+
+#? Practice Exercise 5: 
+# Given a list of roll numbers: [101,105,102,101,108,105,110]. print all unique nums in the list.
+
+# rolls = [101,105,102,101,108,105,110]
+# newRolls = []
+# for roll in rolls:
+#   if roll not in newRolls:
+#     newRolls.append(roll)
+
+# print(newRolls)
+
