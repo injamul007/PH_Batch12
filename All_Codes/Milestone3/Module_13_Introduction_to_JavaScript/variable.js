5b60e53e0b6999e778ce240d6a185e3b1fbc815e
injamul007 <injamul007hoque@gmail.com>
2026-08-14 12:13:42 +0600

    update and add basic js variables in it

--- All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/variable.js
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/variable.js
@@ -2,16 +2,23 @@
 
 // 5 things to declare a variable
 
-var weight = 38;
-var price = 38;
-var year = 1995;
-var money = 2005;
-money = 500;
+// var weight = 38;
+// var price = 38;
+// var year = 1995;
+// var money = 2005;
+// money = 500;
 
-var age;
+// var age;
 
-console.log(money);
-console.log(price);
+// console.log(money);
+// console.log(price);
 
 
-var age = 17;
+// var age = 17;
+
+
+// const num = 10
+// const str = 'hello'
+
+// console.log("this is a num so it will be : ",isNaN(num));
+// console.log("this is a str so it will be : ",isNaN(str));
