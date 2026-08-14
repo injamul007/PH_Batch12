ca2550597c3df3e020b103069d336d73f837e15c
injamul007 <injamul007hoque@gmail.com>
2026-08-14 13:01:12 +0600

    update and add js basic practice problem in it

--- All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/practice_problem.js
+++ All_Codes/Milestone3/Module_13_Introduction_to_JavaScript/practice_problem.js
@@ -8,35 +8,45 @@ const apples1KG = 400
 
 const shopkeeperWillReturn = tkInHand - (oranges1KG + apples1KG)
 
-console.log(shopkeeperWillReturn);
+// console.log(shopkeeperWillReturn);
 
 //? Practice Problem Number 2.
 
 /* Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, of a student */
 
+const mathematics = 75.25
+const biology = 65
+const chemistry = 80
+const physics = 35.45
+const english = 99.50
 
+const averageMarksOfStudent = (mathematics + biology + chemistry + physics + english) / 5
 
 // console.log(Number(averageMarksOfStudent.toFixed(2)));
 
 
 //? Practice Problem Number 3.
 
-/* You task is to divide the given number by 5 and show the remainder as the output */
+/* Your task is to divide the given number by 5 and show the remainder as the output */
 
+const containsNum = 119
 
+const RemainingRemainder = containsNum % 5
 
 // console.log(RemainingRemainder);
 
 
-//? Practice Problem Number 3.
+//? Practice Problem Number 4.
 
 /* What will be the result of the following codes */
 
-// var a = isNaN("11");
+// const a = isNaN("hello");
+// const a = isNaN("11");
 
-// var a = isNaN(2-10);
+// const b = isNaN(2-10);
 
 // console.log(a)
+// console.log(b)
 
 
 // console.log(NaN === NaN); //? this NaN is not even equal to itself
