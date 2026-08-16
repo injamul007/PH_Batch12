125ebedea39e01aaee652624cd60f4f402b3d80b
injamul007 <injamul007hoque@gmail.com>
2026-08-17 03:12:17 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_14_JS_conditionals/nested.js
+++ All_Codes/Milestone3/Module_14_JS_conditionals/nested.js
@@ -1,17 +1,17 @@
 //?
 
-const money = 0;
+// const money = 322;
 
-if (money > 300) {
-  console.log("Bro you are rich !!!")
-} else {
-  if (money > 100) {
-    console.log("tui shala gorib o na abar fokir o na !")
-  } else {
-    if (money > 0) {
-      console.log("dosto kola kha r calcium bara")
-    } else {
-      console.log("amar kase tk dhar chaibi na !!")
-    }
-  }
-}
+// if (money > 300) {
+//   console.log("Bro you are rich !!!")
+// } else {
+//   if(money > 200) {
+//     console.log('you are not rich and not a broke, i say you are middle one');
+//   } else if (money > 100 && money < 200) {
+//     console.log('you are not rich and not a broke, i say you are lower middle one');
+//   } else if( money < 100 && money > 0) {
+//     console.log('you are not rich and not a broke, i say you are very lower one');
+//   } else {
+//     console.log('you are a poor man');
+//   }
+// }
