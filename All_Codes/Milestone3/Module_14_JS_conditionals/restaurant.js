35f28f86fdad5bfee64791bc3170d6e774ffc6d9
injamul007 <injamul007hoque@gmail.com>
2026-08-18 19:34:55 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_14_JS_conditionals/restaurant.js
+++ All_Codes/Milestone3/Module_14_JS_conditionals/restaurant.js
@@ -1,28 +1,28 @@
 //?
 
-const age = 46;
-const foodPrice = 1200;
+// const age = 36;
+// const foodPrice = 1200;
 
-if (age <= 12) {
-  console.log("you can eat for free");
-} else if (age >= 60) {
-  //? 50% discount
-  const discount = foodPrice * 50 / 100;
-  const payAmount = foodPrice - discount;
-  console.log("You have to pay 50% which is :", payAmount);
-} else if (age >= 50 && age <= 59) {
-  //? 30% discount
-  const discount = foodPrice * 30 / 100;
-  const payAmount = foodPrice - discount;
-  console.log("You have to pay 30% which is :", payAmount);
-} else if (age >= 40 && age <= 49) {
-  //? 10% discount
-  const discount = foodPrice * 10 / 100;
-  const payAmount = foodPrice - discount;
-  console.log("You have to pay 10% which is :", payAmount);
-} else{
-  console.log("You have to pay full amount :", foodPrice);
-}
+// if (age <= 12) {
+//   console.log("you can eat for free");
+// } else if (age >= 60) {
+//   //? 50% discount
+//   const discount = foodPrice * 50 / 100
+//   const payAmount = foodPrice - discount
+//   console.log( `you got ${discount} tk discount and you will have to pay ${payAmount}`);
+// } else if (age >= 50 && age <= 59) {
+//   //? 30% discount
+//   const discount = foodPrice * 30 / 100
+//   const payAmount = foodPrice - discount
+//   console.log( `you got ${discount} tk discount and you will have to pay ${payAmount}`);
+// } else if (age > 41 && age <= 49) {
+//   //? 10% discount
+//   const discount = foodPrice * 10 / 100
+//   const payAmount = foodPrice - discount
+//   console.log( `you got ${discount} tk discount and you will have to pay ${payAmount}`);
+// } else {
+//   console.log('you will have to pay full amount which is :', foodPrice);
+// }
 
 
 // if (age <= 12) {
