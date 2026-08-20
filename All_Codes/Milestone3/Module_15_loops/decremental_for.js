a87eca4bc8a47ee68976716722c1f9dda713b49b
injamul007 <injamul007hoque@gmail.com>
2026-08-20 23:15:14 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_15_loops/decremental_for.js
+++ All_Codes/Milestone3/Module_15_loops/decremental_for.js
@@ -7,7 +7,23 @@
 // }
 
 
-for (let i = 20; i>=11; i--) {
-  console.log(i)
-}
+// for (let i = 20; i>=11; i--) {
+//   console.log(i)
+// }
 
+
+
+// for(let i = 10; i>=1; i--) {
+//   console.log(i);
+// }
+
+
+// let sum = 0;
+
+// for(let i = 0; i <= 20; i++) {
+//   if(i % 2 !== 0) {
+//     sum+=i
+//   }
+// }
+
+// console.log('sum of odd number from 1 to 20 is : ', sum);
