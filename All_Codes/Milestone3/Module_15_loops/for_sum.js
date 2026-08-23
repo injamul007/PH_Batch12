bb323358cd94d5089b490bcbdb6cf9986a3623c5
injamul007 <injamul007hoque@gmail.com>
2026-08-23 19:45:36 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_15_loops/for_sum.js
+++ All_Codes/Milestone3/Module_15_loops/for_sum.js
@@ -1,8 +1,16 @@
 //??
 
-let sum = 0;
+// let sum = 0;
+// for(let i= 200; i<=250; i++) {
+//   sum+= i;
+// }
+// console.log(sum);
 
-for(let i= 200; i<=250; i++) {
-  sum+= i;
-}
-console.log(sum);
+
+// let sum = 0;
+
+// for(let i = 200; i <= 250; i++) {
+//   sum+=i
+// }
+
+// console.log(sum);
