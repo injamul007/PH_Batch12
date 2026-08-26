2e604c2b3319ee98340f074ec09c00d0ef90a3f5
injamul007 <injamul007hoque@gmail.com>
2026-08-26 15:39:03 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_15_loops/range.js
+++ All_Codes/Milestone3/Module_15_loops/range.js
@@ -1,13 +1,24 @@
 //?
 
 // let num = 1;
-while (num < 10) {
-  console.log("num----", num);
-  num++;
-}
+// while (num < 10) {
+//   console.log("num----", num);
+//   num++;
+// }
 
-let num = 50;
-while (num <= 100) {
-  console.log("num----", num);
-  num++;
-}
+// let num = 50;
+// while (num <= 100) {
+//   console.log("num----", num);
+//   num++;
+// }
+
+
+// let sum = 0
+// let num = 0
+
+// while(num <= 10) {
+//   sum += num
+//   num++
+// }
+
+// console.log('sum of 10 is :',sum);
