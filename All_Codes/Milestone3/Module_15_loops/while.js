607f8ed5016772662082c084d435faa6f47fa7ba
injamul007 <injamul007hoque@gmail.com>
2026-08-28 00:12:51 +0600

    update and add js basic in it

--- All_Codes/Milestone3/Module_15_loops/while.js
+++ All_Codes/Milestone3/Module_15_loops/while.js
@@ -1,16 +1,19 @@
 //?
 
-const number = 7
-if (number < 10) {
-  console.log("Choto ekta number paisi")
-}
+// const number = 7
+// if (number < 10) {
+//   console.log("Choto ekta number paisi")
+// }
 
-console.log("Something new is cooking")
+// console.log("Something new is cooking")
 
 
 //? -----------While loop-------------
-const num = 0; //? Loop variable
-while (num < 5) {
-  console.log("looping", num);
-  num++;
-}
+// const num = 0; //? Loop variable
+// while (num < 5) {
+//   console.log("looping", num);
+//   num++;
+// }
+
+
+
