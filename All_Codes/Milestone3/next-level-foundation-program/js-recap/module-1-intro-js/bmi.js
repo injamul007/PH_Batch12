e43286861a51721a8c1c7f93cde992379e534e8f
injamul007 <injamul007hoque@gmail.com>
2026-08-29 22:24:31 +0600

    update and add js basics in it

---
+++ All_Codes/Milestone3/next-level-foundation-program/js-recap/module-1-intro-js/bmi.js
@@ -0,0 +1,23 @@
+//? input from terminal
+
+// const weight = process.argv[2]
+// const height = process.argv[3]
+// console.log('weight -->',weight, ', height -->', height);
+
+
+// function calculateBMI (w, h) {
+//   const bmi = w / (h*h)
+//   return bmi
+// }
+
+// const finalCalculate = calculateBMI(weight, height)
+
+// const bmiValue = Number(finalCalculate.toFixed(2));
+
+// if(bmiValue < 18) {
+//   console.log('your bmi is ', bmiValue, 'and you are underweight');
+// } else if (bmiValue > 18 && bmiValue < 24) {
+//   console.log('your bmi is ', bmiValue, 'and you are normal as per your bmi');
+// } else {
+//   console.log('your bmi is ', bmiValue, 'and you are overweight');
+// }
